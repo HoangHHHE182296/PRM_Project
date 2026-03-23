e71ca47e509bc29b0144871767954d535a0eccb0
TruongVu <vuxuantruong170204@gmail.com>
2026-03-23 22:39:03 +0700

    backup api_client

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -3,20 +3,14 @@ import 'package:prm_project/core/configs/environment.dart';
 import 'package:public_openapi/public_openapi.dart';
 
 class ApiClient {
-  static final PublicOpenapi openApi = _createOpenApi();
-
-  static PublicOpenapi _createOpenApi() {
-    final dio = Dio(
+  static final PublicOpenapi openApi = PublicOpenapi(
+    dio: Dio(
       BaseOptions(
         baseUrl: Environment.apiUrl,
         connectTimeout: const Duration(milliseconds: 30000),
         receiveTimeout: const Duration(milliseconds: 30000),
       ),
-    );
-
-    return PublicOpenapi(
-      dio: dio,
-      basePathOverride: Environment.apiUrl,
-    );
-  }
+    ),
+    basePathOverride: Environment.apiUrl,
+  );
 }
